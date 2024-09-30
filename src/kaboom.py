5e54bd293e874aed3c0965f976ad33429de9bd91
Alexander Zatuliveter <alexander.zatuliveter@gmail.com>
2024-09-30 17:28:32 +0600

    modern explosion sprites.

--- src/kaboom.py
+++ src/kaboom.py
@@ -3,23 +3,20 @@ import pygame
 
 class KaBoom:
     def __init__(self, pos):
-        self.frames = [
-            pygame.image.load('./images/kaboom1.png'),
-            pygame.image.load('./images/kaboom2.png'),
-            pygame.image.load('./images/kaboom3.png')
-        ]
-        self.frame_speed_ms = 100
+        self.frames = [pygame.image.load(f'./images/explosion/{n}.png') for n in range(1, 7)]
+        self.frame_speed_ms = 45
         self._start_time = pygame.time.get_ticks()
         self._frame = 0
         self._pos = pos
         self.is_destroyed = False
+        self._frames_count = len(self.frames)
 
     def update(self):
         duration = pygame.time.get_ticks() - self._start_time
         self._frame = duration // self.frame_speed_ms
-        if (self._frame > 2):
+        if (self._frame > self._frames_count):
             self.is_destroyed = True
-        self._frame = min(self._frame, 2)
+        self._frame = min(self._frame, self._frames_count - 1)
 
     def draw(self, screen):
         frame_image = self.frames[self._frame]
